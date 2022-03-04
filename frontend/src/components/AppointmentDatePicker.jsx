348e74ea216fdcd617623b0d0aeda58ce15b5282
Xavier Savoie <xaviersavoie@gmail.com>
2022-03-04 15:12:52 -0500

    implemented datepicker in form

---
+++ frontend/src/components/AppointmentDatePicker.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+
+import 'react-datepicker/dist/react-datepicker.css';
+
+// CSS Modules, react-datepicker-cssmodules.css
+// import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+
+export default function AppointmentDatePicker(props) {
+  const { dateValue, setDateValue } = props;
+  return (
+    <DatePicker
+      id="date"
+      selected={dateValue}
+      onChange={(date) => setDateValue(date)}
+    />
+  );
+}
--- frontend/src/components/AppointmentForm.jsx
+++ frontend/src/components/AppointmentForm.jsx
@@ -1,16 +1,32 @@
-import './AppointmentForm.css'
+import './AppointmentForm.css';
 import { useFormFields } from '../hooks/useFormFields';
+import AppointmentDatePicker from './AppointmentDatePicker';
+import { useEffect, useState } from 'react';
 
 export default function AppointmentForm() {
+  // setup initial date val to block passed days
+  const [dateValue, setDateValue] = useState();
 
-  const [fields, handleFieldChange] = useFormFields({
+  const [fields, handleFieldChange, setValues] = useFormFields({
     fullName: '',
     email: '',
-  })
+    date: '',
+  });
+
+  useEffect(() => {
+    if (dateValue) {
+      const formattedDate = dateValue.toString().split(' ').slice(1, 4).join(' ');
+
+      setValues({
+        ...fields,
+        date: formattedDate,
+      });
+    }
+  }, [dateValue]);
 
   function handleSubmit() {
     // axios post request to backend/new
-    console.log(fields)
+    console.log(fields);
   }
 
   return (
@@ -42,7 +58,12 @@ export default function AppointmentForm() {
           </span>
         </div>
         <div className="time-info">
-          <h4>datePicker</h4>
+        <label>Date</label>
+          <AppointmentDatePicker
+            dateValue={dateValue}
+            setDateValue={setDateValue}
+          />
+          <label>time</label>
           <h4>timePicker</h4>
           <button id="submit-btn" onClick={handleSubmit}>
             Submit
