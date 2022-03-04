9a2d8a77a96d02b611e2204e2d15fa9463195608
Xavier Savoie <xaviersavoie@gmail.com>
2022-03-04 12:56:45 -0500

    Setup appointmentForm component

---
+++ frontend/src/components/AppointmentForm.jsx
@@ -0,0 +1,42 @@
+export default function AppointmentForm() {
+  function handleSubmit() {
+    // axios post request to backend/new
+    console.log("submit");
+  }
+
+  return (
+    <div class="appointment-form">
+      <form>
+        <div class="user-info">
+          <span>
+            <label for="name">Full Name</label>
+            <input
+              value={"test"}
+              onChange={"test"}
+              type="text"
+              name="name"
+              placeholder="Enter Your Name"
+            />
+          </span>
+          <span>
+            <label for="email">Email</label>
+            <input
+              value={"test"}
+              onChange={"test"}
+              type="email"
+              name="email"
+              placeholder="Enter Your Email"
+            />
+          </span>
+        </div>
+        <div class="time-info">
+          <h4>datePicker</h4>
+          <h4>timePicker</h4>
+          <button id="submit-btn" onClick={handleSubmit}>
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
